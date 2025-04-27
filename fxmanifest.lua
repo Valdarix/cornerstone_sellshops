6e3870602361dc6fba26a70972ce830c8b05b135
Cornerstone <james@valdarixgames.com>
2025-04-26 20:28:37 -0400

    refactor(folders): Moved to root

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Cornerstone Scripts'
+name "Cornerstone Sell Shops"
+description 'A simple sell shops script for Ox Inventory'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+}
+
+server_scripts {
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua',   
+}
+
+lua54 'yes'
+use_fxv2_oal 'yes'
